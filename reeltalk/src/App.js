bf108fa6da42751d454545a64c2abea5b3f5d142
Marko Rodic <mrmarkorodic@googlemail.com>
2017-05-10 15:30:56 +0100

    basic home page

--- reeltalk/src/App.js
+++ reeltalk/src/App.js
@@ -6,15 +6,38 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <HomeSection />    
       </div>
-    );
+    )
+  }
+}
+
+class CommentSection extends Component {
+  render() {
+    return (
+      <h1>Comments</h1>
+    )
+  }
+}
+
+class HomeSection extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Read comments while you watch a film</h1>
+        <h4>Just type the name of the film you want to watch and press start at the same time as your film</h4>
+        <input
+          type="text"
+          onChange=
+          {
+            this.onSearchChange
+          }
+        />
+        <span className="results">
+        </span>
+        <button>Start film</button>
+      </div>
+    )
   }
 }
 
